8882cb06b346bec8060883b908dd4a3084d6f06b
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-04-14 17:04:11 +0200

    some styling decision made

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  return <h1>HELLO do you work?</h1>;
+  return (
+    <>
+      <h1>HELLO do you work?</h1>
+      <div>Just some text</div>
+    </>
+  );
 }
 
 export default App;

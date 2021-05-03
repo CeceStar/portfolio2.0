b1805009d022343bce33cc6690dcaf195d129750
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-05-03 16:00:52 +0200

    changed firebase settings.

--- src/assets/data/firebase.config.js
+++ src/assets/data/firebase.config.js
@@ -4,14 +4,15 @@ import "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 let firebaseConfig = {
-  apiKey: "AIzaSyB3J6tMhs1Q8sgJX0gap9gqAw8FWXzFC9c",
-  authDomain: "this-is-me-cece.firebaseapp.com",
-  projectId: "this-is-me-cece",
-  storageBucket: "this-is-me-cece.appspot.com",
-  messagingSenderId: "753134716500",
-  appId: "1:753134716500:web:478c0c47af55ba210a0fc1",
-  measurementId: "G-HB9PZNC6Z3"
+  apiKey: "AIzaSyDMRawuRya3HfGe2xQs0gXxpGqay20Ih-Q",
+  authDomain: "cecilia-hallerby.firebaseapp.com",
+  projectId: "cecilia-hallerby",
+  storageBucket: "cecilia-hallerby.appspot.com",
+  messagingSenderId: "416427913432",
+  appId: "1:416427913432:web:bd3cf2fc8045196ca5e7ad",
+  measurementId: "G-8EZCF8907G"
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);

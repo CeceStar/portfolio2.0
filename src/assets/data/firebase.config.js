94b58aef1c7cce84e11fc8feccf916af9d9fca57
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-05-03 12:50:39 +0200

    changed the firebase settings to another firebase project, so it is on my new google account.

--- src/assets/data/firebase.config.js
+++ src/assets/data/firebase.config.js
@@ -4,14 +4,15 @@ import "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 let firebaseConfig = {
-  apiKey: "AIzaSyAGvTNNPCax7okAx4T9BLBqVRbQPTHqjak",
-  authDomain: "my-personal-page-1373e.firebaseapp.com",
-  projectId: "my-personal-page-1373e",
-  storageBucket: "my-personal-page-1373e.appspot.com",
-  messagingSenderId: "498792809567",
-  appId: "1:498792809567:web:1806d55edfbb68735287ea",
-  measurementId: "G-STJ2MVNQKP"
+  apiKey: "AIzaSyB3J6tMhs1Q8sgJX0gap9gqAw8FWXzFC9c",
+  authDomain: "this-is-me-cece.firebaseapp.com",
+  projectId: "this-is-me-cece",
+  storageBucket: "this-is-me-cece.appspot.com",
+  messagingSenderId: "753134716500",
+  appId: "1:753134716500:web:478c0c47af55ba210a0fc1",
+  measurementId: "G-HB9PZNC6Z3"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();

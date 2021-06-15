19e57b7157b464a1b020122395ec72a155c88e2b
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-06-15 17:38:24 +0200

    removed date temporary

--- src/components/MainPage/BlogPost.jsx
+++ src/components/MainPage/BlogPost.jsx
@@ -4,7 +4,7 @@ const BlogPost = (props) => {
   return (
     <div id={props.id} className="post-container-short">
         <h2>{props.heading}</h2>
-        <p className="date-text">2021-04-28</p>
+        {/* <p className="date-text">2021-04-28</p> */}
         <p className="date-text">{props.text}</p>
     </div>
   );

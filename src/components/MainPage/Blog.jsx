b7967675c63873dfc63dd1e4bff195612d6da10e
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-08-06 12:44:39 +0200

    Added a beachy wave between the pages

--- src/components/MainPage/Blog.jsx
+++ src/components/MainPage/Blog.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import BlogPost from "./BlogPost";
 
 const Blog = ({ allPosts }) => {
--- src/components/MainPage/Presentation.jsx
+++ src/components/MainPage/Presentation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CTAButtons from "./CTAButtons";
 import Sun from "./Sun";
+import PageBreakLine from "./PageBreakLine";
 
 function Presentation() {
   return (
@@ -26,7 +27,8 @@ function Presentation() {
           </p>
           <CTAButtons />
         </div>
-      </div>
+        <PageBreakLine />
+      </div>    
     </>
   );
 }

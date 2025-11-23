f7ea3925a6b0725f1af50f09e027f28b14718333
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2025-11-24 00:20:50 +0100

    fix: revert firebase changes

--- src/assets/data/firebase.config.js
+++ src/assets/data/firebase.config.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase";
+import "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+let firebaseConfig = {
   apiKey: "AIzaSyDMRawuRya3HfGe2xQs0gXxpGqay20Ih-Q",
   authDomain: "cecilia-hallerby.firebaseapp.com",
   projectId: "cecilia-hallerby",
   storageBucket: "cecilia-hallerby.appspot.com",
   messagingSenderId: "416427913432",
   appId: "1:416427913432:web:bd3cf2fc8045196ca5e7ad",
-  measurementId: "G-8EZCF8907G"
+  measurementId: "G-8EZCF8907G",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+const dataBase = firebase.firestore();
+
+export default dataBase;
